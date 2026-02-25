8bebad62917c53a0fcfb942f12b6ad03634e744c
athumaniMfaume <athumanimfaume1995@gmail.com>
2026-02-25 17:12:53 +0300

    another changes in cors and env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-// Determine base URL automatically
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-console.log("API Base URL:", import.meta.env.VITE_API_URL);
-
 const api = axios.create({
-  baseURL: API_BASE,
-  withCredentials: true, // optional if you use cookies
+  baseURL: import.meta.env.VITE_API_URL || "https://tanzania-student-career-guidance-system.onrender.com/api",
 });
 
-// Add token automatically
 api.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem("user"));
   if (user?.token) {
